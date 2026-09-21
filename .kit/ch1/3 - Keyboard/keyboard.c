a4d9f4138fa1c9092ee78c9b4f65d2795928e430
Muhammad Rafiif Ansyadya <muhammadrafiifansyadya@gmail.com>
2026-09-21 14:32:16 +0700

    feat : add kit3

--- .kit/ch1/3 - Keyboard/keyboard.c
+++ .kit/ch1/3 - Keyboard/keyboard.c
@@ -21,3 +21,36 @@ const char keyboard_scancode_1_to_ascii_map[256] = {
       0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
       0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
 };
+
+static struct KeyboardDriverState keyboard_state = {
+    .keyboard_input_on = false,
+    .keyboard_buffer   = 0,
+};
+ 
+void keyboard_state_activate(void) {
+    keyboard_state.keyboard_input_on = true;
+}
+ 
+void keyboard_state_deactivate(void) {
+    keyboard_state.keyboard_input_on = false;
+}
+ 
+void get_keyboard_buffer(char *buf) {
+    *buf = keyboard_state.keyboard_buffer;
+    keyboard_state.keyboard_buffer = 0;
+}
+ 
+void keyboard_isr(void) {
+    /* Wajib dibaca di setiap IRQ1, walau input sedang off, supaya buffer controller tidak penuh. */
+    uint8_t scancode = in(KEYBOARD_DATA_PORT);
+ 
+    if (keyboard_state.keyboard_input_on && !(scancode & KEYBOARD_BREAK_BIT)) {
+        char ascii = keyboard_scancode_1_to_ascii_map[scancode];
+        if (ascii != 0) {
+            keyboard_state.keyboard_buffer = ascii;
+        }
+    }
+ 
+    /* Tanpa ACK, semua IRQ berikutnya tertahan di PIC. Satu-satunya jalur keluar fungsi ini. */
+    pic_ack(IRQ_KEYBOARD);
+}
